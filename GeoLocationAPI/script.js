546167d365c6faf054e1d80c361cdc7b1b6208c8
Chip1505 <connor_pierce@ymail.com>
2023-05-17 13:04:08 -0300

    finished

--- GeoLocationAPI/script.js
+++ GeoLocationAPI/script.js
@@ -2,11 +2,18 @@ navigator.geolocation.getCurrentPosition(
     function(position){
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
+        const coords = [latitude, longitude]
         console.log(latitude, longitude);
 
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        var map = L.map('map').setView(coords, 13);
 
-        
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable')
+            .openPopup();
 
     },
     function(){
