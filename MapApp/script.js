dcf47085d7d836035f1d82b1cb91a2a6a79adb64
Chip1505 <connor_pierce@ymail.com>
2023-05-17 13:47:45 -0300

    Almost done
    just event listener left

--- MapApp/script.js
+++ MapApp/script.js
@@ -10,3 +10,51 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map;
+let mapEvent;
+
+navigator.geolocation.getCurrentPosition(
+    function(position){
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const coords = [latitude, longitude]
+
+        map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        map.on('click', function(mapE){
+            mapEvent = mapE
+            const lat= mapEvent.latlng.lat
+            const lng= mapEvent.latlng.lng
+            console.log(mapEvent)
+    
+                L.marker([lat, lng]).addTo(map)
+                    .bindPopup(L.popup({
+                        maxWidth:250,
+                        minWidth:100,
+                        autoClose:false,
+                        closeOnClick:false,
+                        className:'running-popup',
+                    }))
+                    .setPopupContent('Workout')
+                    .openPopup();
+                
+            form.classList.remove('hidden');
+            inputDistance.focus();
+        })
+
+        
+    },
+    function(){
+        alert("Could not get position.");
+    }
+);
+
+// form event listener to check if submitted/completed
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+})
